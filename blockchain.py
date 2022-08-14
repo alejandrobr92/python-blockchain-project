1c30dee15c7f733a2b1a232bd0154f424c9322e8
alejandrobr <alejandro.bautista@lapieza.io>
2022-08-14 13:58:33 -0500

    Added comments and Doc Strings

--- blockchain.py
+++ blockchain.py
@@ -1,22 +1,30 @@
+# Initializing blockchain list
 blockchain = []
 
 
 def get_last_blockchain_value():
+    """ Return the last value of the current blockchain. """
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blochain value to the blockchain. """
     blockchain.append([last_transaction, transaction_amount])
 
 
-tx_amount = float(input('Your transaction amount: '))
+def get_user_input():
+    """ Returns the in put of the user as a float. """
+    return float(input('Your transaction amount: '))
+
+
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input('Your transaction amount: '))
+tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(),
           transaction_amount=tx_amount)
 
-tx_amount = float(input('Your transaction amount: '))
+tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)
