e008992b638e8bfd8371793896f7ad3524005590
alejandrobr <alejandro.bautista@lapieza.io>
2022-08-14 15:59:48 -0500

    Refactor and use of None keyword.

--- blockchain.py
+++ blockchain.py
@@ -4,11 +4,15 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Return the last value of the current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new value as well as the last blochain value to the blockchain. """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -29,21 +33,21 @@ def print_blockchain_elements():
         print(block)
 
 
-# Get the first transaction input and add the value to the blockchain
-tx_amount = get_transaction_value()
-add_value(tx_amount)
-
 while True:
     print('Please choose')
     print('1: Add a new transactino value')
     print('2: Output the blockchain blocks')
+    print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == 1:
         tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
+        add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == 2:
         print_blockchain_elements()
+    elif user_choice == 'q':
+        break
     else:
         print('Invalid input')
+    print('Choice registered')
 
 print('Done')
